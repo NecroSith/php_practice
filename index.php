5ae57e2426060731fc0061c2982077e7536e025d
Yan_Pustynnyy <necrosith@yandex.ru>
2018-06-19 15:58:29 +0400

    form sending done

--- index.php
+++ index.php
@@ -1,26 +1,30 @@
 <?php
 
-function greeting($name = 'аноним', $day) {
-	switch ($day) {
-		case 'Понедельник':
-		case 'Вторник':
-		case 'Среда':
-		case 'Четверг':
-		case 'Пятница':
-			echo "Привет $name! Хорошего и продуктивного рабочего дня!";
-			break;
-		case 'Суббота':
-		case 'Воскресенье':
-			echo "Привет $name! Желаю вам хорошо отдохнуть в этот день!";
-			break;
-		default:
-			echo "Привет $name! Хорошего тебе дня!";
-			break;
+if (!empty($_POST)) {
+
+	$message = "Вам пришло новое сообщение с сайта от пользователя: " . $_POST["name"] . "\n" . "Email отправителя: " . $_POST["email"] . "\n" . "Текст сообщения: " . $_POST["message"];  
+
+
+	$from = "Email отправителя: info@yanpustynnyy.ru";
+	$sendMail = mail("necrosith@yqandex.ru", "Сообщение с сайта", $message, $from);
+
+	if ($sendMail) {
+		echo "Сообщение успешно отправлено!";
 	}
-	echo "<br>";
+	else {
+		echo "Что то пошло не так :(";
+	}
+
 }
 
-greeting("Ян", "Среда");
-greeting("Ян", "Суббота");
-
 ?>
+
+<link rel="stylesheet" href="main.css">
+<form class="form" action="index.php" method="post">
+
+	<input type="text" name="name" placeholder="Введите ваше имя"><br>
+	<input type="text" name="email" placeholder="Введите вашу почту"><br>
+	<textarea name="message" placeholder="Введите сообщение" id="" cols="30" rows="10"></textarea><br>
+	<input type="submit" value="Отправить сообщение!">
+
+</form>
